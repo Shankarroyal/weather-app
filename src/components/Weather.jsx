2c094565c49d3c0c6439a06fce86a95351b50c93
shankar <shankarroyal8511@gmail.com>
2025-02-11 14:52:56 +0530

    message

---
+++ src/components/Weather.jsx
@@ -0,0 +1,95 @@
+import  { useRef, useEffect } from 'react'
+import { useState } from 'react';
+import './Weather.css'
+import search_icon from './search.png';
+import clear_icon from './clear.png';
+import cloud_icon from './cloud.png';
+import drizzle_icon from './drizzle.png';
+import rain_icon from './rain.png';
+import snow_icon from './snow.png';
+import wind_icon from './wind.png';
+import humidity_icon from './humidity.png';
+
+const Weather = ()=>{
+    const inputRef=useRef()
+    const [weatherData, setWeatherData]=useState(false);
+    const allIcons={
+        "01d":clear_icon,
+        "01n":clear_icon,
+        "02d":cloud_icon,
+        "02n":cloud_icon,
+        "03d":cloud_icon, 
+        "03n":cloud_icon,
+        "04d":drizzle_icon,
+        "04n":drizzle_icon,
+        "09d":rain_icon,
+        "09n":rain_icon,
+        "10d":rain_icon,
+        "10n":rain_icon,
+        "13d":snow_icon,
+        "13n":snow_icon,
+    }
+    const search =async (city)=>{
+        if(city===""){
+            alert("Enter city name");
+            return;
+    }
+        try{
+            const URL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+            const response = await fetch(URL);
+            const data =await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+            console.log(data);
+            const icon=allIcons[data.weather[0].icon]|| clear_icon;
+            setWeatherData({
+                humidity:data.main.humidity,
+                windSpeed:data.wind.speed,
+                temperature:Math.floor(data.main.temp),
+                location:data.name,
+                icon:icon
+            })
+        } catch (error){
+            setWeatherData(false);
+            console.error("Error in fetching weather data");
+
+        }
+    }   
+    useEffect(()=>{search("");
+
+    },[])  
+    
+  return (
+    <div className="Weather">
+        <div className="search-bar">
+            <input  ref={inputRef} type="text" placeholder='search'/>
+            <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
+        </div>
+        {weatherData?<>
+            <img src={weatherData.icon} alt=""  className='weather-icon'/>
+        <p className="temperature">{weatherData.temperature}°c</p>
+        <p className="location">{weatherData.location}</p>
+        <div className="weather-data">
+            <div className="col">
+            <img src={humidity_icon} alt="" />
+            <div>
+                <p>{weatherData.humidity}%</p>
+                <span>Humidity</span>
+            </div>
+            <div className="col">
+            <img src={wind_icon} alt="" />
+            <div>
+                <p>{weatherData.windSpeed}km/h</p>
+                <span>Wind Speed</span>
+            </div>
+        </div>
+    </div>
+    </div>
+        </>:<></>}
+    </div>
+  )
+}
+export default Weather
